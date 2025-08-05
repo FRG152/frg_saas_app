ed217e480b487bd0a7f41ffbe3f2ce52a3d1980b
FlavioRoman <romanflavio3@hotmail.com>
2025-08-04 22:35:39 -0300

    Add CompanionCard, CompanionsList, and Cta components; update Page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,23 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/Cta";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
-      <h1 className="underline">Welcome to my SaaS App</h1>
-      <Button>Let's get started</Button>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <Cta />
+      </section>
     </div>
   );
 };
